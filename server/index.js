b8e10c13163bac63a62d281e667bb961ccade157
Ahmad-Mridul <mridulahmed262@gmail.com>
2025-07-23 23:23:01 +0600

    C:/Program Files/Git/api/jobs-> get http created

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
-require('dotenv').config()
+require("dotenv").config();
 const express = require("express");
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const app = express();
 const cors = require("cors");
 const port = process.env.PORT || 3000;
@@ -7,10 +8,41 @@ const port = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json());
 
-app.get('/api/jobs', async(req,res) => {
-    res.send("Connected");
-})
+const uri =
+    `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.cqkzp8h.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
-app.listen(port,()=>{
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    },
+});
+
+async function run() {
+    try {
+
+        const jobCollection = client.db('job_hunt').collection('jobs');
+
+        app.get('/api/jobs', async(req,res) => {
+            const result = jobCollection.find();
+            const jobs = await result.toArray();
+            res.send(jobs);
+        })
+
+        console.log(
+            "Pinged your deployment. You successfully connected to MongoDB!"
+        );
+    } finally {
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
+app.get("/", async (req, res) => {
+    res.send("Connected");
+});
+
+app.listen(port, () => {
     console.log(`listening from port ${port}`);
-})
+});
